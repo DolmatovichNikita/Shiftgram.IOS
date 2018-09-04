ffb2338bf06d1456a65b6259ce3738ffb2d0d314
DolmatovichNikita <dolmatovich.nikita96@gmail.com>
2018-09-04 22:17:40 +0300

    Add project commit

---
+++ Shiftgram/Shiftgram/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Shiftgram
+//
+//  Created by Nikita on 04.09.2018.
+//  Copyright © 2018 SolIT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Shiftgram/ShiftgramTests/ShiftgramTests.swift
@@ -0,0 +1,36 @@
+//
+//  ShiftgramTests.swift
+//  ShiftgramTests
+//
+//  Created by Nikita on 04.09.2018.
+//  Copyright © 2018 SolIT. All rights reserved.
+//
+
+import XCTest
+@testable import Shiftgram
+
+class ShiftgramTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}

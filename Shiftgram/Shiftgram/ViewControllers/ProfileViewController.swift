70d516f9b68f8fe7c594f018d924f7dd12302dfa
DolmatovichNikita <dolmatovich.nikita96@gmail.com>
2018-09-05 20:42:34 +0300

    Implement register view commit

---
+++ Shiftgram/Shiftgram/ViewControllers/ProfileViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileViewController.swift
+//  Shiftgram
+//
+//  Created by Nikita on 04.09.2018.
+//  Copyright © 2018 SolIT. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}

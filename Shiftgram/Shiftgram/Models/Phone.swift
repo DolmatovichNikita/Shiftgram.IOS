bdf4447b4263cc13f6957fc6b7c0bb12d81d4b74
DolmatovichNikita <dolmatovich.nikita96@gmail.com>
2018-09-05 22:32:15 +0300

    Add Phone model commit

---
+++ Shiftgram/Shiftgram/Models/Phone.swift
@@ -0,0 +1,14 @@
+//
+//  Phone.swift
+//  Shiftgram
+//
+//  Created by Nikita on 05.09.2018.
+//  Copyright © 2018 SolIT. All rights reserved.
+//
+
+import Foundation
+
+struct Phone {
+    let country: String
+    let code: String
+}

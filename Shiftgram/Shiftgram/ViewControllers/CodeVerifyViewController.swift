785f4eaafb703b3291bdaa1b1b9431bb91fa7b28
DolmatovichNikita <dolmatovich.nikita96@gmail.com>
2018-09-05 21:48:44 +0300

    Implement CodeVerify view commit

---
+++ Shiftgram/Shiftgram/ViewControllers/CodeVerifyViewController.swift
@@ -0,0 +1,23 @@
+//
+//  CodeVerifyViewController.swift
+//  Shiftgram
+//
+//  Created by Nikita on 05.09.2018.
+//  Copyright © 2018 SolIT. All rights reserved.
+//
+
+import UIKit
+
+class CodeVerifyViewController: UIViewController {
+
+    @IBOutlet weak var phoneLabel: UILabel!
+    @IBOutlet weak var codeTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}

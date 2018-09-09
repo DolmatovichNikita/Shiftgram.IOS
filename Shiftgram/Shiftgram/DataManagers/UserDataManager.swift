910c3b1d644b7eb3f2e1705731a307767524fc47
DolmatovichNikita <dolmatovich.nikita96@gmail.com>
2018-09-09 18:31:36 +0300

    Add account model commit

---
+++ Shiftgram/Shiftgram/DataManagers/UserDataManager.swift
@@ -0,0 +1,16 @@
+//
+//  UserDataManager.swift
+//  Shiftgram
+//
+//  Created by Nikita on 09.09.2018.
+//  Copyright © 2018 SolIT. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class UserDataManager {
+    
+    private let url = "http://shiftgram.eu-central-1.elasticbeanstalk.com/api/account"
+    
+}
---
+++ Shiftgram/Shiftgram/Models/Account.swift
@@ -0,0 +1,19 @@
+//
+//  Account.swift
+//  Shiftgram
+//
+//  Created by Nikita on 09.09.2018.
+//  Copyright © 2018 SolIT. All rights reserved.
+//
+
+import Foundation
+
+struct Account {
+    let id: Int
+    let firstName: String
+    let lastName: String
+    let bio: String
+    let username: String
+    let photoUrl: String
+    let gender: String
+}

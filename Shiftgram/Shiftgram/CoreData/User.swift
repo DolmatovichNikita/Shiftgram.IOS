0c9312d1892e416bd56fbd03b48bf72ee888fb1c
DolmatovichNikita <dolmatovich.nikita96@gmail.com>
2018-09-09 17:42:34 +0300

    Implement class UserEntity for work with core data commit

---
+++ Shiftgram/Shiftgram/CoreData/User.swift
@@ -0,0 +1,78 @@
+//
+//  User.swift
+//  Shiftgram
+//
+//  Created by Nikita on 09.09.2018.
+//  Copyright © 2018 SolIT. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import CoreData
+
+class UserEntity {
+    
+    private let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    private let context: NSManagedObjectContext
+    
+    init() {
+        context = self.appDelegate.persistentContainer.viewContext
+    }
+    
+    public func addUser(id: Int) {
+        let entity = NSEntityDescription.entity(forEntityName: "User", in: self.context)
+        let user = NSManagedObject(entity: entity!, insertInto: self.context)
+        user.setValue(id, forKey: "id")
+        
+        do {
+         try self.context.save()
+        } catch {
+            print("Failed")
+        }
+    }
+    
+    public func updateUser(value: Any, key: String) {
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "User")
+        
+        do {
+            let entity = try self.context.fetch(request).first as! NSManagedObject
+            entity.setValue(value, forKey: key)
+            try self.context.save()
+        } catch {
+            print("Failed")
+        }
+    }
+    
+    public func deleteUser() {
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "User")
+        let deleteRequest = NSBatchDeleteRequest(fetchRequest: request)
+        
+        do {
+            try self.context.execute(deleteRequest)
+            try self.context.save()
+        } catch {
+            print("Failed")
+        }
+    }
+    
+    public func isExist() ->Bool {
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "User")
+        var isExist = false
+        
+        do {
+            let result = try self.context.fetch(request) as! [NSManagedObject]
+            
+            if result.count > 0 {
+                isExist = true
+            }
+            
+        } catch {
+            print("Failed")
+        }
+        
+        return isExist
+    }
+}

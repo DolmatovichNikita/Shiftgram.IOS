4ab013b5f5fa875f022f655c7639c931e7fa1645
DolmatovichNikita <dolmatovich.nikita96@gmail.com>
2018-09-12 21:38:25 +0300

    Refactoring code commit

--- Shiftgram/Shiftgram/ViewControllers/SettingsViewController.swift
+++ Shiftgram/Shiftgram/ViewControllers/SettingsViewController.swift
@@ -1,23 +1,12 @@
-//
-//  SettingsViewController.swift
-//  Shiftgram
-//
-//  Created by Nikita on 12.09.2018.
-//  Copyright © 2018 SolIT. All rights reserved.
-//
-
 import UIKit
 
 class SettingsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 }

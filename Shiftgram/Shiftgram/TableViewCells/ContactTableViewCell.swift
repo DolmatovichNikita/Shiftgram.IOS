3bfd7cfe89200f6268155753000c23e6423dbd01
DolmatovichNikita <dolmatovich.nikita96@gmail.com>
2018-09-17 21:56:12 +0300

    Implement contact cell commit

---
+++ Shiftgram/Shiftgram/TableViewCells/ContactTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ContactTableViewCell.swift
+//  Shiftgram
+//
+//  Created by Nikita on 17.09.2018.
+//  Copyright © 2018 SolIT. All rights reserved.
+//
+
+import UIKit
+
+class ContactTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

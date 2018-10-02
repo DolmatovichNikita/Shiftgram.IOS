5be32f744f0e952c425c34680a06462cd4454688
DolmatovichNikita <dolmatovich.nikita96@gmail.com>
2018-10-02 20:46:53 +0300

    Implement cell for message view commit

---
+++ Shiftgram/Shiftgram/TableViewCells/MessageTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MessageTableViewCell.swift
+//  Shiftgram
+//
+//  Created by Nikita on 10/2/18.
+//  Copyright © 2018 SolIT. All rights reserved.
+//
+
+import UIKit
+
+class MessageTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

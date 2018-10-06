de43325cd8f1617a8bffc9277947567164b60f8a
DolmatovichNikita <dolmatovich.nikita96@gmail.com>
2018-10-06 14:07:29 +0300

    Implement send audio commit

--- Shiftgram/Shiftgram/Helpers/AwsHelper.swift
+++ Shiftgram/Shiftgram/Helpers/AwsHelper.swift
@@ -2,11 +2,11 @@ import Foundation
 import UIKit
 import AWSCore
 import AWSS3
+import AVFoundation
 
 class AwsHelper {
     
     public static func uploadImage(image: UIImage, completion: @escaping (String) -> Void) {
-        
         let transferManager = AWSS3TransferManager.default()
         let bucketName = "shiftgramimage"
         let jpgImage = UIImageJPEGRepresentation(image, 1)
@@ -37,7 +37,6 @@ class AwsHelper {
     }
     
     public static func downloadImage(path: String, completion: @escaping (NSData) -> Void) {
-        
         let bucketName = "shiftgramimage"
         let key = path
         let transferManager = AWSS3TransferManager.default()
@@ -61,6 +60,55 @@ class AwsHelper {
                 
                 return nil
             }
+        }
+    }
+    
+    public static func uploadRecord(audioRecord: AVAudioRecorder, completion: @escaping (String) -> Void) {
+        let transferManager = AWSS3TransferManager.default()
+        let bucketName = "shiftgramaudio"
+        
+        let uploadRequest = AWSS3TransferManagerUploadRequest()
+        uploadRequest?.body = audioRecord.url
+        uploadRequest?.key = ProcessInfo.processInfo.globallyUniqueString + ".caf"
+        uploadRequest?.bucket = bucketName
+        uploadRequest?.contentType = "audio/caf"
+        
+        transferManager.upload(uploadRequest!).continueWith { (task) -> Any? in
+            if let error = task.error {
+                print(error)
+            } else {
+                DispatchQueue.main.async {
+                    completion((uploadRequest?.key)!)
+                }
+            }
+            
+            return nil
+        }
+    }
+    
+    public static func downloadRecord(path: String, completion: @escaping (NSData) -> Void) {
+        let bucketName = "shiftgramaudio"
+        let key = path
+        let transferManage = AWSS3TransferManager.default()
+        let downloadedFile = URL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent("sound.caf")
+        
+        let downloadedRequest = AWSS3TransferManagerDownloadRequest()
+        downloadedRequest?.bucket = bucketName
+        downloadedRequest?.key = key
+        downloadedRequest?.downloadingFileURL = downloadedFile
+        
+        transferManage.download(downloadedRequest!).continueWith { (task) -> Any? in
+            if let error = task.error {
+                print(error)
+            } else {
+                if let data = NSData(contentsOf: downloadedFile) {
+                    DispatchQueue.main.async {
+                        completion(data)
+                    }
+                }
+            }
+            
+            return nil
         }
     }
 }
--- Shiftgram/Shiftgram/ViewControllers/ChatViewController.swift
+++ Shiftgram/Shiftgram/ViewControllers/ChatViewController.swift
@@ -1,15 +1,19 @@
 import UIKit
-import AVFoundation
 import JSQMessagesViewController
 import ROGoogleTranslate
+import AVFoundation
+import FirebaseStorage
 
-class ChatViewController: JSQMessagesViewController {
+class ChatViewController: JSQMessagesViewController, AVAudioRecorderDelegate, AVAudioPlayerDelegate {
 
     public var conversationName = String()
     public var friendLanguage = String()
     private var messages = [JSQMessage]()
     private let userId = UserEntity().getUserId()
     private var language = (Locale.preferredLanguages.first?.parseLanguage())!
+    
+    var audioRecorder: AVAudioRecorder?
+    var audioPlayer: AVAudioPlayer?
     
     lazy var outgoingBubble: JSQMessagesBubbleImage = {
         return JSQMessagesBubbleImageFactory()!.outgoingMessagesBubbleImage(with: UIColor.jsq_messageBubbleBlue())
@@ -23,8 +27,14 @@ class ChatViewController: JSQMessagesViewController {
         let button = UIButton(frame: CGRect.zero)
         button.setImage(UIImage(named: "microphone"), for: .normal)
         button.imageView?.contentMode = .scaleAspectFit
+        let gestureRecgnizer = UILongPressGestureRecognizer(target: self, action: #selector (longPressedButton(tapGestureRecognizer:)))
+        button.addGestureRecognizer(gestureRecgnizer)
         return button
     }()
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,6 +43,12 @@ class ChatViewController: JSQMessagesViewController {
         collectionView.collectionViewLayout.incomingAvatarViewSize = CGSize.zero
         collectionView.collectionViewLayout.outgoingAvatarViewSize = CGSize.zero
         self.initChat()
+        self.initAudio()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.inputToolbar.contentView.rightBarButtonItem.isEnabled = true
     }
     
     private func initChat() {
@@ -44,78 +60,123 @@ class ChatViewController: JSQMessagesViewController {
         
         _ = query.observe(.childAdded, with: { [weak self] snapshot in
             
-            if  let data        = snapshot.value as? [String: String],
-                let id          = data["sender_id"],
-                let name        = data["name"],
-                let text        = data["sender_id"] == self!.senderId ? data["ownText"] : data["transText"],
-                !text.isEmpty
-            {
-                
-                
-                if let message = JSQMessage(senderId: id, displayName: name, text: text)
-                {
-                    self?.messages.append(message)
+            if let data = snapshot.value as? [String: String] {
+                let id = data["sender_id"]
+                let name = data["name"]
+                let audioUrl = data["audioUrl"]
+                if audioUrl != nil &&  !audioUrl!.isEmpty {
+                    AwsHelper.downloadRecord(path: audioUrl!, completion: { (data) in
+                        let audioItem = JSQAudioMediaItem(data: data as Data)
+                        let message = JSQMessage(senderId: self!.senderId, displayName: self!.senderDisplayName, media: audioItem)
+                        JSQSystemSoundPlayer.jsq_playMessageSentSound()
+                        self!.messages.append(message!)
+                        self?.finishReceivingMessage()
+                    })
+                } else {
+                    let text = data["sender_id"] == self!.senderId ? data["ownText"] : data["transText"]
+                    
+                    if !(text?.isEmpty)! && text != nil {
+                        if let message = JSQMessage(senderId: id, displayName: name, text: text)
+                        {
+                            self?.messages.append(message)
                             
-                    self?.finishReceivingMessage()
+                            self?.finishReceivingMessage()
+                        }
+                    }
                 }
-                
             }
         })
     }
+    
+    private func initAudio() {
+        let fileMgr = FileManager.default
+        let dirPaths = fileMgr.urls(for: .documentDirectory, in: .userDomainMask)
+        let soundFileURL = dirPaths[0].appendingPathComponent("sound.caf")
+        let recordSettings =
+            [AVEncoderAudioQualityKey: AVAudioQuality.min.rawValue,
+             AVEncoderBitRateKey: 16,
+             AVNumberOfChannelsKey: 2,
+             AVSampleRateKey: 44100.0] as [String : Any]
+        let audioSession = AVAudioSession.sharedInstance()
+        
+        do {
+            try audioSession.setCategory(
+                AVAudioSessionCategoryPlayAndRecord)
+        } catch let error as NSError {
+            print("audioSession error: \(error.localizedDescription)")
+        }
+        
+        do {
+            try audioRecorder = AVAudioRecorder(url: soundFileURL,
+                                                settings: recordSettings as [String : AnyObject])
+            audioRecorder?.prepareToRecord()
+        } catch let error as NSError {
+            print("audioSession error: \(error.localizedDescription)")
+        }
+    }
+    
+    @objc private func longPressedButton(tapGestureRecognizer: UILongPressGestureRecognizer) {
+        if tapGestureRecognizer.state == .began {
+            self.initAudio()
+            audioRecorder?.record()
+        }
+        if tapGestureRecognizer.state == .ended {
+            audioRecorder?.stop()
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+            AwsHelper.uploadRecord(audioRecord: self.audioRecorder!) { (value) in
+                let ref = Constants.refs.databaseRoot.child(self.conversationName).childByAutoId()
+                JSQSystemSoundPlayer.jsq_playMessageSentSound()
+                let message = ["sender_id": self.senderId!, "name": self.senderDisplayName, "audioUrl": value] as [String : Any]
+                ref.setValue(message)
+                self.finishSendingMessage()
+                self.inputToolbar.contentView.rightBarButtonItem.isEnabled = true
+            }
+        }
     }
 }
 
 extension ChatViewController {
     
-    override func didPressSend(_ button: UIButton!, withMessageText text: String!, senderId: String!, senderDisplayName: String!, date: Date!)
-    {
-        let ref = Constants.refs.databaseRoot.child(self.conversationName).childByAutoId()
-        
-        let params = ROGoogleTranslateParams(source: language,
-                                             target: self.friendLanguage,
-                                             text:   text)
-        let translator = ROGoogleTranslate()
-        translator.apiKey = "AIzaSyBePVek0atgmg3pzKQyN4oo6a7Oggog3sQ"
-        translator.translate(params: params) { (value) in
-            let message = ["sender_id": senderId, "name": senderDisplayName, "text": value, "ownText": text,
-                           "transText": value]
+    override func didPressSend(_ button: UIButton!, withMessageText text: String!, senderId: String!, senderDisplayName: String!, date: Date!) {
+        if text != nil && text != "" {
+            let ref = Constants.refs.databaseRoot.child(self.conversationName).childByAutoId()
             
-            ref.setValue(message)
+            let params = ROGoogleTranslateParams(source: language,
+                                                 target: self.friendLanguage,
+                                                 text:   text)
+            let translator = ROGoogleTranslate()
+            translator.apiKey = "AIzaSyBePVek0atgmg3pzKQyN4oo6a7Oggog3sQ"
+            translator.translate(params: params) { (value) in
+                let message = ["sender_id": senderId, "name": senderDisplayName, "text": value, "ownText": text,
+                               "transText": value]
+                
+                ref.setValue(message)
+            }
+            self.finishSendingMessage()
         }
-        self.finishSendingMessage()
     }
     
-    override func collectionView(_ collectionView: JSQMessagesCollectionView!, messageDataForItemAt indexPath: IndexPath!) -> JSQMessageData!
-    {
+    override func collectionView(_ collectionView: JSQMessagesCollectionView!, messageDataForItemAt indexPath: IndexPath!) -> JSQMessageData! {
         return messages[indexPath.item]
     }
     
-    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int
-    {
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return messages.count
     }
     
-    override func collectionView(_ collectionView: JSQMessagesCollectionView!, messageBubbleImageDataForItemAt indexPath: IndexPath!) -> JSQMessageBubbleImageDataSource!
-    {
+    override func collectionView(_ collectionView: JSQMessagesCollectionView!, messageBubbleImageDataForItemAt indexPath: IndexPath!) -> JSQMessageBubbleImageDataSource! {
         return messages[indexPath.item].senderId == senderId ? outgoingBubble : incomingBubble
     }
     
-    override func collectionView(_ collectionView: JSQMessagesCollectionView!, avatarImageDataForItemAt indexPath: IndexPath!) -> JSQMessageAvatarImageDataSource!
-    {
+    override func collectionView(_ collectionView: JSQMessagesCollectionView!, avatarImageDataForItemAt indexPath: IndexPath!) -> JSQMessageAvatarImageDataSource! {
         return nil
     }
     
-    override func collectionView(_ collectionView: JSQMessagesCollectionView!, attributedTextForMessageBubbleTopLabelAt indexPath: IndexPath!) -> NSAttributedString!
-    {
+    override func collectionView(_ collectionView: JSQMessagesCollectionView!, attributedTextForMessageBubbleTopLabelAt indexPath: IndexPath!) -> NSAttributedString! {
         return messages[indexPath.item].senderId == senderId ? nil : NSAttributedString(string: messages[indexPath.item].senderDisplayName)
     }
     
-    override func collectionView(_ collectionView: JSQMessagesCollectionView!, layout collectionViewLayout: JSQMessagesCollectionViewFlowLayout!, heightForMessageBubbleTopLabelAt indexPath: IndexPath!) -> CGFloat
-    {
+    override func collectionView(_ collectionView: JSQMessagesCollectionView!, layout collectionViewLayout: JSQMessagesCollectionViewFlowLayout!, heightForMessageBubbleTopLabelAt indexPath: IndexPath!) -> CGFloat {
         return messages[indexPath.item].senderId == senderId ? 0 : 15
     }
 }
-
